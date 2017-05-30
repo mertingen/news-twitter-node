e7c487625dd6a9015fc6f81c93327ca1c11241c9
mert simsek <mertsmsk0@gmail.com>
2017-05-30 18:55:16 +0300

    config file deleted

--- config/config.js
+++
@@ -1,8 +0,0 @@
-var keys = {
-        consumer_key : 'GR7gUwTSc21nrj2GjO1aRwkxW',
-        consumer_secret: 'v3UIX6bBHXc8siHMtzW22bbCesO7LlfAAwGOLxVYUG0PrCLW0J',
-        access_token_key: '2252354330-QiaVZ2OY1tFWJB3UvghqD5yOCDnkoTzfCyVWPs4',
-        access_token_secret: 'f2c5FOJjYQxgWzPCgnpeZpwdmUlspVpvznCK6en9fGGfK'
-}
-
-exports.keys = keys;
